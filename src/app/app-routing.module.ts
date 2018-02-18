8d96a80e7f82d782a4921fc57f0a439587e1a5f1
Anthony Leveillee <aleveillee@gmail.com>
2018-02-18 14:58:58 -0500

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PriceComponent } from './price/price.component';
 
 const routes: Routes = [];
 
